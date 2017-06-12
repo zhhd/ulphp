fce2ecd09307d0eb57351671d0f34f6d27ab6325
zhd <2990488874@qq.com>
2017-06-12 10:01:07 +0800

    优化

---
+++ common/common.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhd
+ * Date: 2017/6/12
+ * Time: 9:54
+ */
--- ulphp/start.php
+++ ulphp/start.php
@@ -5,7 +5,6 @@
  * Date: 2017/6/1
  * Time: 9:50
  */
-
 /**
  * 时区设置
  */
@@ -18,6 +17,11 @@ ini_set("display_errors", "On");
 error_reporting(E_ALL | E_STRICT);
 
 /**
+ * 当前框架版本
+ */
+define('VERSION', '1.1');
+
+/**
  * 常量设置
  */
 define('APP_PATH', __DIR__ . '/../');
@@ -28,19 +32,32 @@ define('APP_PATH', __DIR__ . '/../');
 require __DIR__ . '/core/Autoload.php';
 require __DIR__ . '/core/Exception.php';
 require __DIR__ . '/core/Controller.php';
+
+/**
+ * 自定义帮助函数
+ */
+require __DIR__ . '/../common/common.php';
+
+/**
+ * 加载框架帮助函数
+ */
 require __DIR__ . '/helper.php';
 
 /**
- * 加载数据库帮助类
+ * 自动加载类
  */
-
-
 $autoload = new \ulphp\core\Autoload();
 $autoload->run();
 
+/**
+ * 异常处理类
+ */
 $exception = new \ulphp\core\Exception();
 $exception->run();
 
+/**
+ * 控制器加载类
+ */
 $controller = new \ulphp\core\Controller();
 $controller->load();
 
